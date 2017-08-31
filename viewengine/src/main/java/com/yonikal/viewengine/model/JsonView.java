aba7bb8d1aefd09314d8529ba58bb60fb7f063ac
Yoni Kalangan <yoni.kalangan@gmail.com>
2017-08-31 10:47:33 +0300

    * Started working on a new structure

---
+++ viewengine/src/main/java/com/yonikal/viewengine/model/JsonView.java
@@ -0,0 +1,10 @@
+package com.yonikal.viewengine.model;
+
+
+/**
+ * Created by yoni on 31/08/2017.
+ */
+public class JsonView {
+
+
+}
---
+++ viewengine/src/main/java/com/yonikal/viewengine/model/JsonViewGroup.java
@@ -0,0 +1,7 @@
+package com.yonikal.viewengine.model;
+
+/**
+ * Created by yoni on 31/08/2017.
+ */
+public class JsonViewGroup {
+}

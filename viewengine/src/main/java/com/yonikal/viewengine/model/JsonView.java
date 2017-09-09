9e16e1112214fe1187966b143771315965d6737c
Yoni Kalangan <yoni.kalangan@gmail.com>
2017-09-09 18:58:44 +0300

    * Regular class params

--- viewengine/src/main/java/com/yonikal/viewengine/model/JsonView.java
+++ viewengine/src/main/java/com/yonikal/viewengine/model/JsonView.java
@@ -1,10 +1,14 @@
 package com.yonikal.viewengine.model;
 
 
+import android.view.View;
+
 /**
  * Created by yoni on 31/08/2017.
  */
 public class JsonView {
 
-
+    // Class properties
+    public static final int MATCH_PARENT = -1;
+    public static final int WRAP_CONTENT = -2;
 }
